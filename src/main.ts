af6e9750f18d8a5cb350cbc18466cca648230a2b
s00fT <kurevinss@gmail.com>
2025-05-03 13:39:51 +0300

    origin

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
 
   app.enableCors({
     credentials: true,
-    origin: ['http://localhost:3001', 'https://shopclientwithout.onrender.com'],
+    origin: ['http://localhost:3001', 'https://shopclientwithout-production.up.railway.app'],
   });
 
   const config = new DocumentBuilder()
